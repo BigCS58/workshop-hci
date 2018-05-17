edefc26246a661923b3e938026ae4444477a811d
BIG_CS <thurakit.big@gmail.com>
2018-05-17 16:41:19 +0700

    push

--- js/custom-buy.js
+++ js/custom-buy.js
@@ -8,12 +8,16 @@ $(document).ready(function () {
 function loadDataTable() {
     var rows = ''
     data.forEach(element => {
+
         rows += `
             <tr>
-                <td>${element.name}</td>
-                <td>${element.price}</td>
-                <td>${element.amount}</td>
                 <td>${element.date}</td>
+                <td>${element.name}</td>
+                <td>${element.amount}</td>
+                <td>${element.unit}</td>
+                <td>${element.amountPerunit}</td>
+                <td>${element.price}</td>
+                <td ${element.status == 'ยังไม่จัดส่ง' ? 'style=\"color:red\"' : 'style=\"color:green\"'}>${element.status}</td>
             </tr>
             `
     });
@@ -22,10 +26,17 @@ function loadDataTable() {
 }
 
 var data = [
-    { name: 'a', price: 200, amount: 20, date: '18/20/2016' },
-    { name: 'b', price: 200, amount: 20, date: '18/20/2016' },
-    { name: 'c', price: 200, amount: 20, date: '18/20/2016' },
-    { name: 'd', price: 200, amount: 20, date: '18/20/2016' },
-    { name: 'e', price: 200, amount: 20, date: '18/20/2016' },
-    { name: 'f', price: 200, amount: 20, date: '18/20/2016' },
+    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ลัง', amountPerunit: 14, price: 500, date: '18/20/2016', status: 'จัดส่งแล้ว' },
+    { name: 'สุราขาว40ดีกรี 330มล.', amount: 50, unit: 'ลัง', amountPerunit: 12, price: 1000, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
+    { name: 'สุราผสมเสือดำ 625มล.', amount: 30, unit: 'ลัง', amountPerunit: 10, price: 700, date: '18/20/2016', status: 'จัดส่งแล้ว' },
+    { name: 'สุราผสมเสือดำ 330มล.', amount: 20, unit: 'ลัง', amountPerunit: 12, price: 300, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
+    { name: 'สุราแสงโสมกลม 700มล.', amount: 30, unit: 'ลัง', amountPerunit: 15, price: 500, date: '18/20/2016', status: 'จัดส่งแล้ว' },
+    { name: 'สุราแสงโสมแบน 300มล.', amount: 40, unit: 'ลัง', amountPerunit: 15, price: 600, date: '18/20/2016', status: 'จัดส่งแล้ว' },
+    { name: 'สุราหงส์ทองกลม 700มล.', amount: 40, unit: 'ลัง', amountPerunit: 12, price: 700, date: '18/20/2016', status: 'จัดส่งแล้ว' },
+    { name: 'สุราหงส์ทองแบน 350มล.', amount: 30, unit: 'ลัง', amountPerunit: 14, price: 900, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
+    { name: 'สุราเบลนด์285 700มล.', amount: 50, unit: 'ลัง', amountPerunit: 12, price: 1200, date: '18/20/2016', status: 'จัดส่งแล้ว' },
+    { name: 'เบียร์สิงห์ใหญ่ 630มล.', amount: 100, unit: 'ลัง', amountPerunit: 12, price: 500, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
+    { name: 'มาม่า ต้มยำกุ้ง 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 300, date: '18/20/2016', status: 'จัดส่งแล้ว' },
+    { name: 'มาม่า ต้มยำกุ้ง น้ำข้น 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 400, date: '18/20/2016', status: 'จัดส่งแล้ว' },
+    { name: 'มาม่า หมูสับ 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 350, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
 ];
