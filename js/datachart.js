639396165a942c48da8d53d8b55b3d0df524ec93
BIG_CS <thurakit.big@gmail.com>
2018-05-18 20:37:19 +0700

    add chart2

--- js/datachart.js
+++ js/datachart.js
@@ -5,89 +5,89 @@ var categoryAxis;
 var chartData = 
 [
   {
-	  "country": "USA",
-		"visits": 4252,
+	  "month": "USA",
+		"percentage": 4252,
     "color": "#FF0F00"
 	},
 	{
-		"country": "China",
-		"visits": 1882,
+		"month": "China",
+		"percentage": 1882,
     "color": "#FF6600"
 	}, 
 	{
-		"country": "Japan",
-		"visits": 1809,
+		"month": "Japan",
+		"percentage": 1809,
     "color": "#FF9E01"
 	}, 
 	{
-	  "country": "Germany",
-		"visits": 1322,
+	  "month": "Germany",
+		"percentage": 1322,
     "color": "#FCD202"
 	}, 
 	{
-		"country": "UK",
-		"visits": 1122,
+		"month": "UK",
+		"percentage": 1122,
     "color": "#F8FF01"
 	}, 
 	{
-		"country": "France",
-		"visits": 1114,
+		"month": "France",
+		"percentage": 1114,
     "color": "#B0DE09"
 	}, 
 	{
-		"country": "India",
-		"visits": 984,
+		"month": "India",
+		"percentage": 984,
     "color": "#04D215"
     
 	}, 
 	{
-		"country": "Spain",
-		"visits": 711,
+		"month": "Spain",
+		"percentage": 711,
     "color": "#0D8ECF"
 	}, 
 	{
-		"country": "Netherlands",
-		"visits": 665,
+		"month": "Netherlands",
+		"percentage": 665,
     "color": "#0D52D1"
 	}, 
 	{
-		"country": "Russia",
-		"visits": 580,
+		"month": "Russia",
+		"percentage": 580,
     "color": "#2A0CD0"
 	}, 
 	{
-		"country": "South Korea",
-		"visits": 443,
+		"month": "South Korea",
+		"percentage": 443,
     "color": "#8A0CCF"
 	}, 
 	{
-		"country": "Canada",
-		"visits": 441,
+		"month": "Canada",
+		"percentage": 441,
     "color": "#CD0D74"
 	}, 
 	{
-		"country": "Brazill",
-		"visits": 395,
+		"month": "Brazill",
+		"percentage": 395,
     "color": "#754DEB"
 	}, 
 	{
-	  "country": "Italy",
-		"visits": 386,
+	  "month": "Italy",
+		"percentage": 386,
     "color": "#DDDDDD"
 	}, 
 	{
-		"country": "Australia",
-		"visits": 384,
+		"month": "Australia",
+		"percentage": 384,
     "color": "#999999"
 	}, 
 	{
-		"country": "Taiwan",
-		"visits": 338,
+		"month": "Taiwan",
+		"percentage": 338,
     "color": "#333333"
 	}, 
 	{
-		"country": "Poland",
-		"visits": 328,
+		"month": "Poland",
+		"percentage": 328,
     "color": "#000000"
 	}
 ];
@@ -96,7 +96,7 @@ var chartData =
 AmCharts.ready(function () {
   chart = new AmCharts.AmSerialChart();
 	chart.dataProvider = chartData;
-	chart.categoryField = "country";
+	chart.categoryField = "month";
   chart.position = "left";
   chart.angle = 30;
 	chart.depth3D = 15;
@@ -111,7 +111,7 @@ AmCharts.ready(function () {
   
     
 	graph = new AmCharts.AmGraph();
-	graph.valueField = "visits";
+	graph.valueField = "percentage";
 	graph.type = "column";	
   graph.colorField = "color";
 	graph.lineAlpha = 0;
