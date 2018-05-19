479bdbb940dde59c68e0e9f570157725038a5497
BIG_CS <thurakit.big@gmail.com>
2018-05-19 09:50:18 +0700

    finish

--- js/custom-buy.js
+++ js/custom-buy.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
-    $( "#date-add" ).datepicker({
-        format: 'dd/mm/yyyy',
-        regional: "th" , 
-    });
-    $.datepicker.setDefaults($.datepicker.regional['th']);
+
 
     loadDataTable();
     $('#data-buy').DataTable({
@@ -36,7 +32,7 @@ function loadDataTable() {
                 <td>${element.price}</td>
                 <td ${element.status == 'ยังไม่จัดส่ง' ? 'style=\"color:red\"' : 'style=\"color:green\"'}>${element.status}</td>
                 <td style="text-align:center;">
-                    <button class="btn btn-success" data-toggle="modal" data-target="#myModal" contenteditable="false">แก้ไข</button>
+                    <button class="btn btn-warning" data-toggle="modal" data-target="#myModal" contenteditable="false">แก้ไข</button>
                     <button class="btn btn-danger"   data-toggle="modal" data-target="#deletedModal" onclick="deletedData(this)">ลบ</button>
                 </td>
             </tr>
@@ -52,17 +48,17 @@ function loadDataTable() {
 
 
 var data = [
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ลัง', amountPerunit: 14, price: 500, date: '18/20/2016', status: 'จัดส่งแล้ว' },
-    { name: 'สุราขาว40ดีกรี 330มล.', amount: 50, unit: 'ลัง', amountPerunit: 12, price: 1000, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
-    { name: 'สุราผสมเสือดำ 625มล.', amount: 30, unit: 'ลัง', amountPerunit: 10, price: 700, date: '18/20/2016', status: 'จัดส่งแล้ว' },
-    { name: 'สุราผสมเสือดำ 330มล.', amount: 20, unit: 'ลัง', amountPerunit: 12, price: 300, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
-    { name: 'สุราแสงโสมกลม 700มล.', amount: 30, unit: 'ลัง', amountPerunit: 15, price: 500, date: '18/20/2016', status: 'จัดส่งแล้ว' },
-    { name: 'สุราแสงโสมแบน 300มล.', amount: 40, unit: 'ลัง', amountPerunit: 15, price: 600, date: '18/20/2016', status: 'จัดส่งแล้ว' },
-    { name: 'สุราหงส์ทองกลม 700มล.', amount: 40, unit: 'ลัง', amountPerunit: 12, price: 700, date: '18/20/2016', status: 'จัดส่งแล้ว' },
-    { name: 'สุราหงส์ทองแบน 350มล.', amount: 30, unit: 'ลัง', amountPerunit: 14, price: 900, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
-    { name: 'สุราเบลนด์285 700มล.', amount: 50, unit: 'ลัง', amountPerunit: 12, price: 1200, date: '18/20/2016', status: 'จัดส่งแล้ว' },
-    { name: 'เบียร์สิงห์ใหญ่ 630มล.', amount: 100, unit: 'ลัง', amountPerunit: 12, price: 500, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
-    { name: 'มาม่า ต้มยำกุ้ง 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 300, date: '18/20/2016', status: 'จัดส่งแล้ว' },
-    { name: 'มาม่า ต้มยำกุ้ง น้ำข้น 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 400, date: '18/20/2016', status: 'จัดส่งแล้ว' },
-    { name: 'มาม่า หมูสับ 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 350, date: '18/20/2016', status: 'ยังไม่จัดส่ง' },
+    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ลัง', amountPerunit: 14, price: 500, date: '18/10/2561', status: 'จัดส่งแล้ว' },
+    { name: 'สุราขาว40ดีกรี 330มล.', amount: 50, unit: 'ลัง', amountPerunit: 12, price: 1000, date: '17/10/2561', status: 'ยังไม่จัดส่ง' },
+    { name: 'สุราผสมเสือดำ 625มล.', amount: 30, unit: 'ลัง', amountPerunit: 10, price: 700, date: '17/10/2561', status: 'จัดส่งแล้ว' },
+    { name: 'สุราผสมเสือดำ 330มล.', amount: 20, unit: 'ลัง', amountPerunit: 12, price: 300, date: '17/09/2561', status: 'ยังไม่จัดส่ง' },
+    { name: 'สุราแสงโสมกลม 700มล.', amount: 30, unit: 'ลัง', amountPerunit: 15, price: 500, date: '15/09/2561', status: 'จัดส่งแล้ว' },
+    { name: 'สุราแสงโสมแบน 300มล.', amount: 40, unit: 'ลัง', amountPerunit: 15, price: 600, date: '15/08/2561', status: 'จัดส่งแล้ว' },
+    { name: 'สุราหงส์ทองกลม 700มล.', amount: 40, unit: 'ลัง', amountPerunit: 12, price: 700, date: '15/08/2561', status: 'จัดส่งแล้ว' },
+    { name: 'สุราหงส์ทองแบน 350มล.', amount: 30, unit: 'ลัง', amountPerunit: 14, price: 900, date: '15/08/2561', status: 'ยังไม่จัดส่ง' },
+    { name: 'สุราเบลนด์285 700มล.', amount: 50, unit: 'ลัง', amountPerunit: 12, price: 1200, date: '14/07/2561', status: 'จัดส่งแล้ว' },
+    { name: 'เบียร์สิงห์ใหญ่ 630มล.', amount: 100, unit: 'ลัง', amountPerunit: 12, price: 500, date: '14/07/2561', status: 'ยังไม่จัดส่ง' },
+    { name: 'มาม่า ต้มยำกุ้ง 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 300, date: '14/07/2561', status: 'จัดส่งแล้ว' },
+    { name: 'มาม่า ต้มยำกุ้ง น้ำข้น 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 400, date: '18/07/2561', status: 'จัดส่งแล้ว' },
+    { name: 'มาม่า หมูสับ 55กรัม', amount: 20, unit: 'แพ็ค', amountPerunit: 40, price: 350, date: '14/07/2561', status: 'ยังไม่จัดส่ง' },
 ];
--- js/custom-sell.js
+++ js/custom-sell.js
@@ -16,6 +16,9 @@ function loadDataTable() {
                 <td>${element.amount}</td>
                 <td>${element.unit}</td>
                 <td>${element.price}</td>
+                <td style="text-align:center;">
+                    <button class="btn btn-danger"   data-toggle="modal" data-target="#deletedModal" >ลบ</button>
+                </td>
             </tr>
             `
     });
@@ -24,15 +27,15 @@ function loadDataTable() {
 }
 
 var data = [
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
-    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/20/2016 18:01:00' },
+    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/05/2561 18:02:00' },
+    { name: 'เรือดำน้ำ', amount: 50, unit: 'คัน', price: 500, datetime: '18/05/2561 17:40:00' },
+    { name: 'รถถัง', amount: 50, unit: 'คัน', price: 500, datetime: '18/05/2561 16:28:00' },
+    { name: 'เรือดำน้ำ', amount: 50, unit: 'คัน', price: 500, datetime: '18/04/2561 16:27:00' },
+    { name: 'เรือดำน้ำ', amount: 50, unit: 'คัน', price: 500, datetime: '18/04/2561 16:26:00' },
+    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/04/2561 16:25:00' },
+    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/04/2561 16:24:00' },
+    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/03/2561 14:44:00' },
+    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/03/2561 14:20:00' },
+    { name: 'สุราขาว40ดีกรี 625มล.', amount: 50, unit: 'ขวด', price: 500, datetime: '18/03/2561 14:10:00' },
+    { name: 'รถถัง', amount: 50, unit: 'คัน', price: 500, datetime: '18/03/2561 12:01:00' },
 ];
